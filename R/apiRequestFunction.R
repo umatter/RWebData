b350c65d7127124c2b26366380880837f382d734
Ulrich Matter <ulrich.matter@unibas.ch>
2015-07-06 15:37:56 +0200

    Removed apiRequestFunction() from export (only use as internal function), added exports for S4 classes and methods (important for installation of package)

--- R/apiRequestFunction.R
+++ R/apiRequestFunction.R
@@ -1,39 +1,39 @@
 
-##' Writes a function to generate API requests
-##' 
-##' An extended version of generateURLFunction(). The generated function returns an object of class 
-##' "apirequest".
-##' @usage apiRequestFunction(x, base.url, multiparam=NULL, key.param=NA,
-##'  key.object=NA, vectorizeIt=FALSE)
-##' @param x a two-column data frame or a named list containing the parameter names and values 
-##' (see details).
-##' @param base.url a character string containing the basic url for the api.
-##' @param multiparam a character string with the name of a parameter that can take multiple input 
-##' values in the resulting function  (defaults to NULL).
-##' @param key.param a character string containing the api key parameter required in the url.
-##' @param key.object the name of the object the api.key is saved in (as character).
-##' @param vectorizeIt logical, indicating whether the resulting function should contain an implicit
-##' loop over parameters with no default value (i.e., allows vectors as inputs). Default is FALSE. 
-##' TRUE only works if x is a data frame.      
-##' @return an object of class apirequest.
-##' @details x (if a data frame) should contain the parameter names in the first column and 
-##' respective default-values in the second column (both as character strings). Parameters that 
-##' have no default value have NAs in the second column.The function attempts to get the api-key 
-##' from the environment "apikeys" (therefore has to be defined there before; see saveAPIkey()).
-##' if x is a list, the parametern names values are provided like in a function call, i.e., 
-##' list(param1=value.param1, param2=...). 
-##' The option of providing x as a list ist basically implemented for getTabularData() 
-##' (direct individual requests to an API, no vectorization).
-##' @export
-##' @examples
-##' # First, make sure the necessary API key is saved in your R session:
-##' # (This example is based on the Project Vote Smart API [PVS API])
-##' saveAPIkey(key.var="pvs", key="YOUR-KEY-HERE")
-##' pvsmeasure <- "http://api.votesmart.org/Measure.getMeasure?"
-##' measureparameters <- data.frame(parameter="measureId", value=NA)
-##' \dontrun{getMeasureRequest <- apiRequestFunction(x=measureparameters, base.url=pvsmeasure,
-##' key.param="key",key.object="pvs")}
-##' 
+## Writes a function to generate API requests
+## 
+## An extended version of generateURLFunction(). The generated function returns an object of class 
+## "apirequest".
+## @usage apiRequestFunction(x, base.url, multiparam=NULL, key.param=NA,
+##  key.object=NA, vectorizeIt=FALSE)
+## @param x a two-column data frame or a named list containing the parameter names and values 
+## (see details).
+## @param base.url a character string containing the basic url for the api.
+## @param multiparam a character string with the name of a parameter that can take multiple input 
+## values in the resulting function  (defaults to NULL).
+## @param key.param a character string containing the api key parameter required in the url.
+## @param key.object the name of the object the api.key is saved in (as character).
+## @param vectorizeIt logical, indicating whether the resulting function should contain an implicit
+## loop over parameters with no default value (i.e., allows vectors as inputs). Default is FALSE. 
+## TRUE only works if x is a data frame.      
+## @return an object of class apirequest.
+## @details x (if a data frame) should contain the parameter names in the first column and 
+## respective default-values in the second column (both as character strings). Parameters that 
+## have no default value have NAs in the second column.The function attempts to get the api-key 
+## from the environment "apikeys" (therefore has to be defined there before; see saveAPIkey()).
+## if x is a list, the parametern names values are provided like in a function call, i.e., 
+## list(param1=value.param1, param2=...). 
+## The option of providing x as a list ist basically implemented for getTabularData() 
+## (direct individual requests to an API, no vectorization).
+## @export
+## @examples
+## # First, make sure the necessary API key is saved in your R session:
+## # (This example is based on the Project Vote Smart API [PVS API])
+## saveAPIkey(key.var="pvs", key="YOUR-KEY-HERE")
+## pvsmeasure <- "http://api.votesmart.org/Measure.getMeasure?"
+## measureparameters <- data.frame(parameter="measureId", value=NA)
+## \dontrun{getMeasureRequest <- apiRequestFunction(x=measureparameters, base.url=pvsmeasure,
+## key.param="key",key.object="pvs")}
+## 
 
 
 
