f9895eee3c3eff330bcb5cdd3a6231472572e8d6
umatter <umatter@protonmail.com>
2018-01-22 18:42:23 +0100

    update license notice

--- R/listToDataFrame.R
+++ R/listToDataFrame.R
@@ -1,3 +1,20 @@
+# This file is part of RWebData.
+# 
+# RWebData is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# RWebData is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with RWebData.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 ## Extract data in a tabular form (data-frames) from a tree-structure (nested list based on parsed web data)
 ## 
 ## Tree-data mapping algorithm: first extract entity types then stack entities within each type and save
